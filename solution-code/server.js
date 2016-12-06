26472417948b238c95fd04a8ec9b421ebbe8946e
ZebGirouard <timzebgir@gmail.com>
2016-12-05 21:06:30 -0700

    Adding some more comments

--- solution-code/server.js
+++ solution-code/server.js
@@ -6,7 +6,7 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Set up DB
+//Set up DB -- look at those models
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/airplanes");
 var Schema = mongoose.Schema;
@@ -16,18 +16,18 @@ var NerdSchema = new Schema({
 });
 var Nerd = mongoose.model('Nerd', NerdSchema);
 
-//Set up EJS
+//Set up EJS -- look at those views
 app.set('views', __dirname);
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 //Nerd form
-app.get('/nerds/new', function(req, res) {
-	res.sendFile(__dirname+'/newNerd.html');
+app.get('/nerds/new', function(req, res) { //look at that controller
+	res.sendFile(__dirname+'/newNerd.html'); 
 });
 
 //Add new nerd
-app.post('/nerds', function(req, res) {
+app.post('/nerds', function(req, res) { //and look at that controller
 	Nerd.create({name: req.body.name, title: req.body.title}, function(error, nerd) {
 		res.render('nerd_show', {nerd: nerd});
 	});
